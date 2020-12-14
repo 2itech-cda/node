5c94655938461cc4d371dd438bc86733371b6d44
Vincent Letourneau <vincent@nanoninja.com>
2020-12-14 17:29:57 +0100

    Configure express

---
+++ webapp/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// name=toto&email=toto@gmail.com&password=0000
+// req.body.name, req.body.email, req.body.password
+
+app.get('/', (_req, res) => {
+    res.send('<h1>Express Application</h1>');
+});
+
+// http://localhost:3000/users/chucknorris?age=100
+app.get('/users/:name', (req, res) => {
+    console.log('req.query.age:', req.query.age);
+    console.log('req.params.name:', req.params.name);
+
+    res.send('<h1>Simple Response</h1>');
+});
+
+app.post('/users', (req, res) => {
+    console.log(req.body.name, req.body.email);
+    res.json(req.body);
+});
+
+app.get('/about', (_req, res) => {
+    res.json({"title": "about"});
+});
+
+app.listen(3000, () => console.log('Server is running...'));
