8d979a9e3ab93b4b5fdcf86da1c79a4738692cc6
Vincent Letourneau <vincent@nanoninja.com>
2020-12-14 11:34:26 +0100

    Add flag base

---
+++ system/flag.js
@@ -0,0 +1,11 @@
+// node flag.js -name toto -age 100 -ville lyon
+
+// getFlag('name')  toto
+// getFlag('age')   100
+// getFlag('ville') lyon
+function getFlag(flagName) {
+    return '';
+}
+
+console.log(getFlag('name')); // toto
+// console.log(process.argv);
