49c9c19a02dc8310b78a98ab56ff2a518d30ff3d
Vincent Letourneau <vincent@nanoninja.com>
2020-12-15 17:18:45 +0100

    CSS Task done

--- sass-task/gulpfile.js
+++ sass-task/gulpfile.js
@@ -1,13 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
 
 /**
  * outputStyle: Expanded, compressed
  */
 function css() {
-    return gulp.src('./scss/**/*.scss')
-            .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-            .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./scss/**/*.scss')            // Source des fichiers à traiter
+            .pipe(plumber())                       // Previent les erreurs
+            .pipe(sass({outputStyle: 'expanded'})) // .on('error', sass.logError))
+            .pipe(postcss([autoprefixer]))         // ::-moz-placeholder
+            .pipe(gulp.dest('./dist/css'))         // sauvegarde du css normal
+            .pipe(rename({suffix: '.min'}))        // main.min.css
+            .pipe(postcss([cssnano]))              // minification du css
+            .pipe(gulp.dest('./dist/css'));        // sauvegarde du css autopréfixé et compressé
 }
 
-gulp.task('css', css);
+function watcher() {
+    gulp.watch('./scss/**/*.scss', css);
+}
+
+gulp.task('css', done => {
+    css();
+    done();
+});
+
+gulp.task('watch', gulp.parallel(watcher));
