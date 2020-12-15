91a419d2bfe9e4a32713cd46375b2bbc6e0930f5
Vincent Letourneau <vincent@nanoninja.com>
2020-12-15 11:09:43 +0100

    Add apiKey middleware

--- webapp/index.js
+++ webapp/index.js
@@ -5,6 +5,22 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(morgan('dev'));
+
+// http://localhost:3000/contact?key=abcd
+
+// wrapping, closure
+function apiKey(key) {
+    return (req, res, next) => {
+        if (!req.query.key || req.query.key !== key) {
+            res.status(401).send('<h1>Unauthorized</h1>');
+            return;
+        }
+        next();
+    }
+}
+
+app.use(apiKey('abcd'));
+
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
