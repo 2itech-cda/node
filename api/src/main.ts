7fa9878079bc57a22ed27a4c678a74137c6cd7eb
Vincent Letourneau <vincent@nanoninja.com>
2020-12-16 13:07:36 +0100

    Database connection

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,23 @@
 import * as express from 'express';
+import { createConnection } from 'typeorm';
+import * as bodyParser from 'body-parser'; 
 import { Request, Response, Application } from 'express';
 
 const app: Application = express();
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response): void => {
     res.json({message: 'hello'});
 });
 
-app.listen(3000, () => console.log('Server is running...'));
+app.listen(3000, () => {
+    console.log(`[express] server has started on localhost:3000`);
+
+    try {
+        createConnection();
+    } catch (error) {
+        console.log('[typeorm] connection error:', error);
+    }
+});
